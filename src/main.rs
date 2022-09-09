dcd298003b15689554e0b0f1311458adf8f88005
Raul Martinez <raulmartinezm@users.noreply.github.com>
2022-09-09 17:18:55 +0200

    Add subscription persistence.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgresql");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind port 8000");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,18 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpServer};
+use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, pg_pool: PgPool) -> Result<Server, std::io::Error> {
+    let pg_pool = web::Data::new(pg_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscribe", web::post().to(subscribe))
+            .app_data(pg_pool.clone())
     })
     .listen(listener)?
     .run();
